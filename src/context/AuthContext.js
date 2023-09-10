7547dd1c849a4be77f895eb235194ed7300cee6d
Miguel <lordellomiguel9@gmail.com>
2023-09-10 14:00:31 -0300

    Feat: PostDashboard function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,6 +9,46 @@ export const AuthProvider = ({ children }) => {
   const [userToken, setUserToken] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
 
+  const postDashboard = async () => {
+    try {
+      let userInfo = await AsyncStorage.getItem("userInfo");
+      userInfo = JSON.parse(userInfo);
+      const userId = userInfo.uuid;
+      
+      // Obtém o token de autorização armazenado
+      const authorizationHeader = await AsyncStorage.getItem("userToken");
+      
+      const response = await axios.post(`http://192.168.0.102:8080/api/v1/dashboard/${userId}`, null, {
+        headers: {
+          Authorization: authorizationHeader,
+        },
+      });
+  
+      if (response.status !== 200) {
+        throw new Error("Network response was not ok");
+      }
+  
+      const body = response.data;
+  
+      // Verifica se as informações foram retornadas corretamente
+      if (body) {
+        setDashboardInfo(body);
+        AsyncStorage.setItem("dashboardInfo", JSON.stringify(body));
+        console.log("Dashboard Info: ", body);
+  
+        setDashboardId(body.dashboardId);
+        AsyncStorage.setItem("dashboardID", body.dashboardId.toString()); // Converte para string se necessário
+        console.log("Dashboard ID: ", body.dashboardId);
+      } else {
+        console.error("Dashboard data is missing in the response.");
+      }
+  
+      return body;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const login = async (username, senha) => {
     try {
       const response = await axios.post(`http://192.168.0.102:8080/login`, {
@@ -30,6 +70,7 @@ export const AuthProvider = ({ children }) => {
       AsyncStorage.setItem("userInfo", JSON.stringify(body));
       console.log("Response Body:", body);
 
+      postDashboard()
       return body;
     } catch (error) {
       console.error(error);
@@ -68,7 +109,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoading, userToken, login, logout }}>
+    <AuthContext.Provider value={{ isLoading, userToken, userInfo, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
